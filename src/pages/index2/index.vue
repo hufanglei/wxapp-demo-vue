6ffd69e5c1fac53bfc15ff56345181a966eb30e7
hufanglei <690328661@qq.com>
2025-09-30 08:30:26 +0800

    init

---
+++ src/pages/index2/index.vue
@@ -0,0 +1,44 @@
+<template>
+ <view class="content">
+		<view class="rpx">rpx</view>
+		<view class="VW">vw</view>
+		<view class="sass">sass</view>
+	</view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hello',
+    }
+  },
+  onLoad() {},
+  methods: {},
+}
+</script>
+
+
+<style lang="scss">
+/*rpx 小程序中的单位 750rpx = 屏幕的宽度 */
+.rpx {
+	width: 750px;
+	height: 100px;
+	background-color: aqua;
+}
+/*vw h5单位 100vw = 屏幕的宽度100vh=屏幕的高度半**/
+.VW {
+   width: 50vw;
+   height: 100px;
+   background-color: tan;
+}
+
+.content {
+  .sass{
+     background-color: red;
+     color: $uni-color-primary;
+  }
+}
+
+
+</style>
